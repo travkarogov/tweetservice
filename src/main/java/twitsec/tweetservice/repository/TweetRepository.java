58831be7789ee0995dda29c291f94fab910089c3
Travka Rogov <twan.vanantwerpen@student.fontys.nl>
2020-06-13 13:47:56 +0200

    Loose coupling werkend

--- src/main/java/twitsec/tweetservice/repository/TweetRepository.java
+++ src/main/java/twitsec/tweetservice/repository/TweetRepository.java
@@ -7,9 +7,19 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import twitsec.tweetservice.entity.Tweet;
 
+import java.util.Optional;
+
 @CrossOrigin("*")
 public interface TweetRepository extends PagingAndSortingRepository<Tweet, Integer> {
 
     @RestResource(path = "/byProfileId")
     Page<Tweet> getTweetsByProfileId(Pageable pageable, int profileId);
+
+    @Override
+    @RestResource(exported = false)
+    Page<Tweet> findAll(Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    Optional<Tweet> findById(Integer integer);
 }
