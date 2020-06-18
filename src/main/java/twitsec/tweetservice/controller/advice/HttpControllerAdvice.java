b0dece6a60b88724fd5b16364b0f236b4427246e
Travka Rogov <twan.vanantwerpen@student.fontys.nl>
2020-06-18 21:33:46 +0200

    Forgot import of class

--- src/main/java/twitsec/tweetservice/controller/advice/HttpControllerAdvice.java
+++ src/main/java/twitsec/tweetservice/controller/advice/HttpControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import twitsec.tweetservice.controller.exception.NoTokenProvidedException;
 import twitsec.tweetservice.controller.exception.NotAuthorizedException;
 import twitsec.tweetservice.service.exception.TokenInvalidException;
 
